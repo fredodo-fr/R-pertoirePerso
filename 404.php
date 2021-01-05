be8ee36685e398d84a6453034774e67f50de3092
fredodo-fr <fr.r.mercier@gmail.com>
2021-01-05 20:30:42 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@
     <body>
 
     <p>Erreur 404, Désolé nous n'avons pas trouvé votre page !</p>
-    <a href="page/article_liste.php">Clikez ici pour retourner à la page principale</a>
+    <a href="page/article_liste.php">Cliquez ici pour retourner à la page principale</a>
 
     </body>
 
